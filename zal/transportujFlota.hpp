94e9ed51f0e994ee18be1b0d135b13122c6b12d8
FigurskiAleksy <aleksy.figurski@gmail.com>
2020-11-21 22:17:28 +0000

    update

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,31 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    int zaglowki = 0;
+
+    if(towar == 0)
+    {
+      return 0;
+    }
+      else
+    {
+
+      int towar_ile =0;
+
+      while (towar_ile < towar)
+      {
+        Stocznia stocznia{}
+
+        Statek* statek1 = stocznia();
+
+        towar_ile = towar_ile + statek1->transportuj();
+
+        if (statek1 == dynamic_cast<Zaglowiec>(statek1))
+        {
+          zaglowki++;
+        }
+        delete statek1;
+      }
+      return zaglowki
+    }
 }
